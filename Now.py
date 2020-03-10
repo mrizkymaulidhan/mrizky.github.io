6342b311678bd9d09c1e95cae7b5825f0bb0b01a
mrizkymaulidhan <mrizky04@gmail.com>
2020-03-10 22:16:16 +0700

    Update Fix

--- Now.py
+++ Now.py
@@ -3,10 +3,11 @@ import datetime
 import json
 from bs4 import BeautifulSoup
 
-republika={
+Terkini={
     "time":"",
-    "category":"",
-    "tittle":""
+    "kategori":"",
+    "judul":"",
+    "tanggal":""
 }
 
 data = []
@@ -14,14 +15,16 @@ data = []
 page = requests.get("https://www.republika.co.id/")
 obj = BeautifulSoup(page.text,'html.parser');
 
-for headline in obj.find_all('div', class_='bungkus_txt_headline'):
-    republika["category"] = (headline.find('h1').text)
-    republika["tittle"] = (headline.find('h2').text)
+for headline in obj.find_all('div', class_='conten1'):
+    Terkini["kategori"] = headline.find('h1').text
+    Terkini["judul"] = headline.find('h2').text
+    Terkini["tanggal"] = headline.find('div', class_='date').text
     date = datetime.datetime.now()
     today = date.strftime("%A")+", "+date.strftime("%d")+" "+date.strftime("%B")+" "+date.strftime("%Y")
-    republika["time"] = today
+    Terkini["time"] = today
 
-    data.append (str(republika))
+    data.append (dict(Terkini))
+    print(Terkini)
 
-with open ("Now.json", "w") as write_file:
-    json.dump(data, write_file)
+with open ("Terkini.json", "w") as write_file:
+    json.dump(data, write_file, indent=4)
